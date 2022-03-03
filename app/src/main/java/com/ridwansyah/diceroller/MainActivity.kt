6861807a85f8eceb82855872496ea82906b84f80
ridwans99 <ridwansyah899@gmail.com>
2022-03-03 16:22:43 +0700

    push image dice roller

--- app/src/main/java/com/ridwansyah/diceroller/MainActivity.kt
+++ app/src/main/java/com/ridwansyah/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ridwansyah.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -28,8 +29,27 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
 
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+//        diceImage.setImageResource(R.drawable.dice_2)
+//        when (diceRoll) {
+//            1 -> diceImage.setImageResource(R.drawable.dice_1)
+//            2 -> diceImage.setImageResource(R.drawable.dice_2)
+//            3 -> diceImage.setImageResource(R.drawable.dice_3)
+//            4 -> diceImage.setImageResource(R.drawable.dice_4)
+//            5 -> diceImage.setImageResource(R.drawable.dice_5)
+//            6 -> diceImage.setImageResource(R.drawable.dice_6)
+//        }
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
