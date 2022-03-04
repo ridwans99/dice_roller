786924e5e285684c3d128ed0b2b3c4093b23d2d7
ridwans99 <ridwansyah899@gmail.com>
2022-03-04 23:24:42 +0700

    2 dice roller

--- app/src/main/java/com/ridwansyah/diceroller/MainActivity.kt
+++ app/src/main/java/com/ridwansyah/diceroller/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val diceRoll2 = dice.roll2()
 
         // Update the screen with the dice roll
         val diceImage: ImageView = findViewById(R.id.imageView)
@@ -39,23 +40,55 @@ class MainActivity : AppCompatActivity() {
 //            5 -> diceImage.setImageResource(R.drawable.dice_5)
 //            6 -> diceImage.setImageResource(R.drawable.dice_6)
 //        }
-        val drawableResource = when (diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+        when (diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+//        val drawableResource = when (diceRoll) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
+
+//        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        when (diceRoll2){
+            1 -> diceImage2.setImageResource(R.drawable.dice_1)
+            2 -> diceImage2.setImageResource(R.drawable.dice_2)
+            3 -> diceImage2.setImageResource(R.drawable.dice_3)
+            4 -> diceImage2.setImageResource(R.drawable.dice_4)
+            5 -> diceImage2.setImageResource(R.drawable.dice_5)
+            6 -> diceImage2.setImageResource(R.drawable.dice_6)
         }
 
-        diceImage.setImageResource(drawableResource)
-        diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
+//        val drawableResource = when (diceRoll2) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
     }
 }
 
 class Dice(private val numSides: Int) {
 
     fun roll(): Int {
+        return (1..numSides).random()
+    }
+
+    fun roll2(): Int {
         return (1..numSides).random()
     }
 }
